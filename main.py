33eac753d0a996131197cf48c5e182b0af52f344
tomwink8 <tomwinkelhorst08@gmail.com>
2026-08-25 17:35:58 +0200

    Produktliste hinzugefügt

--- main.py
+++ main.py
@@ -2,3 +2,27 @@ name = input("Wie heißt du? ")
 
 print(f"Hallo {name}!")
 print("Ich lerne gerade Python und Git.") 
+
+
+produkte = [
+    {
+        "name": "Apfel",
+        "preis": 1.99
+    },
+     {
+        "name": "Gurke",
+        "preis": 0.99
+    },
+    {
+        "name": "Banane",
+        "preis": 2.49
+    }
+]
+
+
+def zeige_produkte():
+    for produkt in produkte:
+        print(f"{produkt['name']}: {produkt['preis']:.2f} €")
+
+
+zeige_produkte()
