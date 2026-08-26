b201fe5d382bbfa6d546aa4c174a7f4e5a57a56d
tomwink8 <tomwinkelhorst08@gmail.com>
2026-08-26 20:47:34 +0200

    Plural hinzugefügt

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ print("Ich lerne gerade Python und Git.")
 produkte = [
     {
         "name": "Apfel",
+        "plural": "Äpfel",
         "preis": 1.99,
         "artikel": "der"
     },
     {
         "name": "Gurke",
+        "plural": "Gurken",
         "preis": 0.99,
         "artikel": "die"
     },
     {
         "name": "Banane",
+        "plural": "Bananen",
         "preis": 2.49,
         "artikel": "die"
     }
@@ -37,12 +40,12 @@ def zeige_produkte():
 
 zeige_produkte()
 
-def berechne_gesamtpreis():
-    gesamtpreis = sum(produkt['preis'] for produkt in produkte)
-    return gesamtpreis
+#def berechne_gesamtpreis():
+    #gesamtpreis = sum(produkt['preis'] for produkt in produkte)
+    #return gesamtpreis
 
-gesamtpreis = berechne_gesamtpreis()
-print(f"Gesamtpreis: {gesamtpreis:.2f} €")
+#gesamtpreis = berechne_gesamtpreis()
+#print(f"Gesamtpreis: {gesamtpreis:.2f} €")
 
 warenkorb = []
 
@@ -54,22 +57,32 @@ def kaufe_produkt():
 
             menge = int(input("Wie viele möchtest du kaufen? "))
 
-            # Prüfen, ob das Produkt bereits im Warenkorb ist
             for artikel in warenkorb:
                 if artikel["name"] == produkt["name"]:
                     artikel["menge"] += menge
-                    print(f"Du hast {menge} weitere {produkt['name']} gekauft.")
+
+                    if menge == 1:
+                        kaufartikel_text = kaufartikel(produkt["artikel"])
+                        print(f"Du hast {kaufartikel_text} {produkt['name']} gekauft.")
+                    else:
+                        print(f"Du hast {menge} weitere {produkt['plural']} gekauft.")
+
                     return
 
-            # Wenn das Produkt noch nicht im Warenkorb ist
             warenkorb.append({
                 "name": produkt["name"],
+                "plural": produkt["plural"],
                 "preis": produkt["preis"],
                 "artikel": produkt["artikel"],
                 "menge": menge
             })
 
-            print(f"Du hast {menge} {produkt['name']} gekauft.")
+            if menge == 1:
+                kaufartikel_text = kaufartikel(produkt["artikel"])
+                print(f"Du hast {kaufartikel_text} {produkt['name']} gekauft.")
+            else:
+                print(f"Du hast {menge} {produkt['plural']} gekauft.")
+
             return
 
     print("Produkt nicht gefunden.")
@@ -88,20 +101,25 @@ while weiter != "nein":
         print('Bitte antworte mit "ja" oder "nein".')
         weiter = input("Möchtest du noch etwas kaufen? ").lower()
 
+
+
 print("\nWarenkorb:")
 
 for produkt in warenkorb:
-    einzelpreis = produkt["preis"]
-    menge = produkt["menge"]
-    gesamt = einzelpreis * menge
+    gesamt = produkt["preis"] * produkt["menge"]
+    print(f"{produkt['menge']}x {produkt['name']} - {gesamt:.2f} €")
 
-    print(f"{menge}x {produkt['name']} - {gesamt:.2f} €")
+    
 
 def berechne_gesamtpreis():
+
     gesamtpreis = sum(
         produkt["preis"] * produkt["menge"]
         for produkt in warenkorb
     )
 
     return gesamtpreis
+
+gesamtpreis = berechne_gesamtpreis()
+print(f"Gesamtpreis: {gesamtpreis:.2f} €")
     
